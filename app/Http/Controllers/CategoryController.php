e5421bee66feb90c64ba1c6e39e470eb2906ae95
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-07-02 13:59:08 +0600

    delete

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -78,6 +78,8 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return redirect()->back()->with(['success' => 'Category deleted']);
     }
 }
