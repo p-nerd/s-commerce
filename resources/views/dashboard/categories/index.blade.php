0cf2d861a1b984faa59b05c48f054336fd9f1c34
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-07-04 12:35:05 +0600

    tailwind merge

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -1,12 +1,83 @@
 <x-dashboard-layout>
     <x-slot name="header">
-        <div class="flex justify-between">
-            <x-dash.title>
-                @if (isset($category))
+        <x-dash.title>
+            @if (isset($category))
                     Sub-categories of '{{ $category->name }}'
                 @else
                     Categories
-                @endif
+                @endif</x-table.title>
+            <x-dash.new-button href="{{ route('dashboard.products.create') }}">
+                Create new product
+            </x-dash.new-button>
+    </x-slot>
+
+    <x-table.filters-row>
+        <x-table.search-input />
+        <x-table.select-per-page />
+    </x-table.filters-row>
+
+    @if ($products->isEmpty())
+        <x-table.empty>You didn't create any product yet</x-table.empty>
+    @else
+        <x-table.table>
+            <x-table.thead>
+                <x-table.tr>
+                    <x-table.th>No</x-table.th>
+                    <x-table.sortable-th name="name">Name</x-table.sortable-th>
+                    <x-table.th>Description</x-table.th>
+                    <x-table.th name="price">Price</x-table.th>
+                    <x-table.th name="discount_price">Discount Price</x-table.th>
+                    <x-table.sortable-th name="category">Category</x-table.sortable-th>
+                    <x-table.sortable-th name="stock">Stock</x-table.sortable-th>
+                    <x-table.th></x-table.th>
+                </x-table.tr>
+            </x-table.thead>
+            <x-table.tbody>
+                @php
+                    $isDesc = request()->query('order') === 'desc';
+                    $no = $isDesc ? $products->toArray()['to'] : $products->toArray()['from'];
+                @endphp
+
+                @foreach ($products as $product)
+                    <x-table.tr>
+                        <x-table.td>
+                            {{ $no }}
+
+                            @php
+                                if ($isDesc) {
+                                    $no--;
+                                } else {
+                                    $no++;
+                                }
+                            @endphp
+                        </x-table.td>
+                        <x-table.td>{{ $product->name }}</x-table.td>
+                        <x-table.td>{{ $product->short_description }}</x-table.td>
+                        <x-table.td>৳{{ $product->price }}</x-table.td>
+                        <x-table.td>{{ $product->discount_price ? "৳{$product->discount_price}" : '-' }}</x-table.td>
+                        <x-table.td>{{ $product->category->name }}</x-table.td>
+                        <x-table.td>{{ $product->stock }}</x-table.td>
+                        <x-table.actions-td>
+                            <x-table.view-button
+                                href="{{ route('dashboard.products.show', ['product' => $product->id]) }}" />
+                            <x-table.edit-button
+                                href="{{ route('dashboard.products.edit', ['product' => $product->id]) }}" />
+                            <x-table.delete-button
+                                href="{{ route('dashboard.products.destroy', $product) }}" />
+                        </x-table.actions-td>
+                    </x-table.tr>
+                @endforeach
+            </x-table.tbody>
+        </x-table.table>
+        <x-table.pagination :data="$products" />
+    @endif
+</x-dashboard-layout>
+
+<x-dashboard-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <x-dash.title>
+
             </x-dash.title>
             <x-dash.anchor-link href="{{ route('dashboard.categories.create') }}">Create new
                 category</x-dash.anchor-link>
