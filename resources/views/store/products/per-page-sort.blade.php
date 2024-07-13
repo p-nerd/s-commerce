1845f0a3771df322fe8c31110971bff620dc3363
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-07-13 13:38:10 +0600

    filters and pagination finally done

--- resources/views/store/products/per-page-sort.blade.php
+++ resources/views/store/products/per-page-sort.blade.php
@@ -4,24 +4,36 @@
         <div class="sort-by-cover mr-10">
             <div class="sort-by-product-wrap">
                 <div class="sort-by">
-                    <span><i class="fi-rs-apps"></i>Show:</span>
+                    <span>
+                        <i class="fi-rs-apps"></i>
+                        Show:
+                    </span>
                 </div>
                 <div class="sort-by-dropdown-wrap">
                     <span>
-                        <span id="show-per-page"
-                            data-per-page="{{ $products->perPage() }}">{{ $products->perPage() }}</span>
+                        <span id="show-per-page" data-per-page="{{ $products->perPage() }}">
+                            {{ $products->perPage() }}
+                        </span>
                         <i class="fi-rs-angle-small-down"></i>
                     </span>
                 </div>
             </div>
             <div class="sort-by-dropdown">
                 <ul>
-                    <li><a class="per-page-select-link active" data-per-page="50">50</a></li>
-                    <li><a class="per-page-select-link" data-per-page="100">100</a></li>
-                    <li><a class="per-page-select-link" data-per-page="150">150</a></li>
-                    <li><a class="per-page-select-link" data-per-page="200">200</a></li>
                     <li>
-                        <a class="per-page-select-link" data-per-page="{{ $products->total() }}">All</a>
+                        <a class="per-page-link" data-per-page="50">50</a>
+                    </li>
+                    <li>
+                        <a class="per-page-link" data-per-page="100">100</a>
+                    </li>
+                    <li>
+                        <a class="per-page-link" data-per-page="150">150</a>
+                    </li>
+                    <li>
+                        <a class="per-page-link" data-per-page="200">200</a>
+                    </li>
+                    <li>
+                        <a class="per-page-link" data-per-page="{{ $products->total() }}">All</a>
                     </li>
                 </ul>
             </div>
@@ -29,20 +41,35 @@
         <div class="sort-by-cover">
             <div class="sort-by-product-wrap">
                 <div class="sort-by">
-                    <span><i class="fi-rs-apps-sort"></i>Sort by:</span>
+                    <span>
+                        <i class="fi-rs-apps-sort"></i>
+                        Sort by:
+                    </span>
                 </div>
                 <div class="sort-by-dropdown-wrap">
-                    <span><span id="show-sort" data-filter="featured">Featured</span> <i
-                            class="fi-rs-angle-small-down"></i></span>
+                    <span>
+                        <span id="show-filter" data-filter="{{ request('filter') ?? 'featured' }}">Featured</span>
+                        <i class="fi-rs-angle-small-down"></i>
+                    </span>
                 </div>
             </div>
             <div class="sort-by-dropdown">
                 <ul>
-                    <li><a class="sort-link active" data-filter="featured">Featured</a></li>
-                    <li><a class="sort-link" data-filter="low-to-high">Price: Low to High</a></li>
-                    <li><a class="sort-link" data-filter="high-to-low">Price: High to Low</a></li>
-                    <li><a class="sort-link" data-filter="release-date">Release Date</a></li>
-                    <li><a class="sort-link" data-filter="rating">Avg. Rating</a></li>
+                    <li>
+                        <a class="filter-link" data-filter="featured">Featured</a>
+                    </li>
+                    <li>
+                        <a class="filter-link" data-filter="low-to-high">Price: Low to High</a>
+                    </li>
+                    <li>
+                        <a class="filter-link" data-filter="high-to-low">Price: High to Low</a>
+                    </li>
+                    <li>
+                        <a class="filter-link" data-filter="release-date">Release Date</a>
+                    </li>
+                    <li>
+                        <a class="filter-link" data-filter="rating">Avg. Rating</a>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -51,34 +78,34 @@
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
-        const perPageSelectLinks = document.querySelectorAll('.per-page-select-link');
-        const sortLinks = document.querySelectorAll(".sort-link");
+        const perPageLinks = document.querySelectorAll('.per-page-link');
+        const filterLinks = document.querySelectorAll(".filter-link");
 
         const products = document.querySelector('#products');
-        const itemsCount = document.querySelector('#items-count');
         const showPerPage = document.querySelector('#show-per-page')
-        const showSort = document.querySelector("#show-sort");
+        const showFilter = document.querySelector("#show-filter");
 
-        perPageSelectLinks.forEach(link => {
+
+
+        perPageLinks.forEach(link => {
             link.addEventListener('click', async function(event) {
                 event.preventDefault();
 
                 const perPage = this.getAttribute('data-per-page');
-                const filter = showSort.getAttribute('data-filter');
+                const filter = showFilter.getAttribute('data-filter');
+
                 const url = `/products?filter=${filter}&per_page=${perPage}`;
+
                 try {
                     const response = await fetch(url, {headers: {"X-Type": "partial"}});
                     const data = await response.text();
 
                     products.innerHTML = data;
-                    showPerPage.innerHTML = perPage == '{{ $products->total() }}' ? "All" :
-                        perPage;
+
+                    showPerPage.innerHTML = this.innerHTML;
                     showPerPage.setAttribute("data-per-page", perPage);
 
-                    link.classList.add("active");
-
-                    perPageSelectLinks.forEach(l => l.classList.remove("active"));
-
+                    perPageLinks.forEach(l => l.classList.remove("active"));
                     this.classList.add("active");
 
                     history.pushState(null, '', url);
@@ -88,15 +115,14 @@
             });
         });
 
-        sortLinks.forEach(link => {
+        filterLinks.forEach(link => {
             link.addEventListener('click', async function(event) {
                 event.preventDefault();
 
                 const perPage = showPerPage.getAttribute("data-per-page");
                 const filter = this.getAttribute('data-filter');
 
-                const url =
-                    `/products?filter=${filter}&per_page=${perPage}`;
+                const url = `/products?filter=${filter}&per_page=${perPage}`;
 
                 try {
                     const response = await fetch(url, {headers: {"X-Type": "partial"}});
@@ -104,11 +130,11 @@
 
                     products.innerHTML = data;
 
-                    showSort.setAttribute("data-filter", filter);
-                    showSort.innerText = this.innerText;
+                    showFilter.setAttribute("data-filter", filter);
+                    showFilter.innerHTML = this.innerHTML;
 
 
-                    sortLinks.forEach(l => l.classList.remove("active"));
+                    filterLinks.forEach(l => l.classList.remove("active"));
                     link.classList.add("active");
 
                     history.pushState(null, '', url);
@@ -117,6 +143,20 @@
                 }
             });
         });
+
+
+        perPageLinks.forEach(l => {
+            if (l.getAttribute("data-per-page") === showPerPage.getAttribute("data-per-page")) {
+                l.classList.add("active")
+            }
+        });
+
+        filterLinks.forEach(l => {
+            if (l.getAttribute("data-filter") === showFilter.getAttribute("data-filter")) {
+                l.classList.add("active")
+            }
+        });
+
 
     });
 </script>
